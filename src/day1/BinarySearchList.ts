2f20b2f681549479f3f85f1d54141f25439519af
Dominik Eilenberger <dominik.eilenberger@hotmail.com>
2023-06-30 17:46:36 +0200

    add binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length
 
+    while (low < high) {
+        let mid = Math.floor(low + (high - low) / 2)
+        let value = haystack[mid]
+
+        if (value === needle) {
+            return true
+        }
+        else if (needle < value) {
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+    }
+    return false;
 }
