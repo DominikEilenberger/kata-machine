f0f82bb0dfdb69d6d26ff539891e6da225144c57
Dominik Eilenberger <dominik.eilenberger@hotmail.com>
2023-07-09 13:04:29 +0200

    add Binary Search (inOrder, preOrder, postOrder)

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,25 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    // 1. base case
+    if (!curr) {
+        return path;
+    }
 
+    // 2. recurse
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    // nothing to do here
+
+    return path;
+
+}
+
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
